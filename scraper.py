1072e2f8f3f92c61fb53aefb4bc6d38250892763
ryry91021 <ryry91021@gmail.com>
2024-08-22 15:56:28 -0400

    cleaned data

--- scraper.py
+++ scraper.py
@@ -1,12 +1,10 @@
 #Web scraping application
 import requests #HTTP requests
-from bs4 import BeautifulSoup #Soup object fro, HTML response
+from bs4 import BeautifulSoup #Soup object for, HTML response
 import pandas as pd
 from io import StringIO
+import matplotlib as mpl
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-}
 def main():
     url="https://www.basketball-reference.com/teams/NYK/2024.html"
     response=requests.get(url)
@@ -19,6 +17,10 @@ def main():
     #print(statsPG)
 
     statsDF = pd.read_html(StringIO(str(statsPG)))[0]  # Use StringIO to wrap the HTML string
+    
+
+    #drop rank
+    statsDF=statsDF.drop('Rk', axis=1)
     print(statsDF)
 
 if __name__=="__main__":
